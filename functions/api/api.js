9c5c4d77366bb58966955ff0e41b5e1ba84b2ff3
vayvaychicken <107634935+vayvaychicken@users.noreply.github.com>
2024-01-02 10:29:23 -0500

    .

--- functions/api/api.js
+++ functions/api/api.js
@@ -2,8 +2,12 @@ const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const router = express.Router();
+const fs = require('fs');
+const path = require('path')
 
 let records = [];
+
+app.set('view engine', 'ejs');
 
 //Get all students
 router.get('/', (req, res) => {
@@ -26,24 +30,14 @@ router.put('/', (req, res) => {
 });
 
 //showing demo records
-router.get('/demo', (req, res) => {
-  res.json([
-    {
-      id: '001',
-      name: 'Smith',
-      email: 'smith@gmail.com',
-    },
-    {
-      id: '002',
-      name: 'Sam',
-      email: 'sam@gmail.com',
-    },
-    {
-      id: '003',
-      name: 'lily',
-      email: 'lily@gmail.com',
-    },
-  ]);
+router.get('/hello', (req, res) => {
+  console.log(fs.existsSync(path.join(__dirname, "data.txt")))
+  fs.readFile(path.join(__dirname, "data.txt"), 'utf8', function(err, data) {
+    if (err) throw err;
+    console.log("yes", data)
+    res.send(data)
+  })
+  //res.send("hello")
 });
 
 app.use('/.netlify/functions/api', router);
